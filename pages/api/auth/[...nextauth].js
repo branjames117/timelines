8d56bdf6f010507d62de88e747931717b7732909
Brandon Hoskins <branjames117@gmail.com>
2023-02-19 16:26:45 -0600

    add secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   // configure authentication providers
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -14,8 +15,5 @@ export const authOptions = {
     strategy: 'jwt',
   },
 };
-
-console.log(process.env.GOOGLE_ID);
-console.log(process.env.GOOGLE_SECRET);
 
 export default NextAuth(authOptions);
