98431426c853f957026d222a2a6954ef53904433
Brandon Hoskins <branjames117@gmail.com>
2023-02-25 16:36:46 -0600

    experiment

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import Container from '../components/Container';
 import '../styles/globals.scss';
@@ -6,13 +5,6 @@ import '../styles/globals.scss';
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='description' content='User-generated timeline app' />
-        <meta name='og:title' content='Timelines' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <title>Timelines</title>
-      </Head>
       <Container>
         <Component {...pageProps} />
       </Container>
