1ecd76ec1799833e591869c870279f316a0315bf
Brandon Hoskins <branjames117@gmail.com>
2023-02-25 21:38:22 -0600

    experiment

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='description' content='User-generated timeline app' />
-        <meta name='og:title' content='Timelines' />
-        <meta name='twitter:card' content='summary_large_image' />
         <title>Timelines</title>
       </Head>
       <SessionProvider session={pageProps.session}>
