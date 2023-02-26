1542e89b71e66a82f414df59ac3ce5843772f384
Brandon Hoskins <branjames117@gmail.com>
2023-02-25 21:59:00 -0600

    experiment

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
-import { Head } from 'next/document';
 import { SessionProvider } from 'next-auth/react';
 import Container from '../components/Container';
 import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Timelines</title>
-      </Head>
-      <SessionProvider session={pageProps.session}>
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      </SessionProvider>
-    </>
+    <SessionProvider session={pageProps.session}>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </SessionProvider>
   );
 }
